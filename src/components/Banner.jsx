26dcc41212d226f3420dd4ba5e67a64bfb8d598e
Sumit <sk7627577@gmail.com>
2023-12-19 14:07:52 +0530

    task to remove heading

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -78,11 +78,17 @@ const Banner = () => {
 
                   />
                 </span>
-                <br />In Just
+                {/* task to remove */}
+                {/* <br />In Just */}
+
               </h2>
+                {/* task to remove */}
               {/* <h2 className='heading'>In Just</h2> */}
             </div>
-            <h1 class="minute">1 Minute</h1>
+                {/* task to remove */}
+            {/* <h1 class="minute">1 Minute</h1> */}
+            <br/>
+            <br/>
 
             <p >
 

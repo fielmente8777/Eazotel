fa73ad2c7910ae9ff1ead5a1af8a28616ae753ec
Yash0125 <116873935+Yash0125@users.noreply.github.com>
2024-01-29 13:47:29 +0530

    style : teamjoin updated

--- src/components/TeamJoin.jsx
+++ src/components/TeamJoin.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import '../style/TeamJoin.css';
 import { FaArrowRight } from "react-icons/fa";
+import teamjoinbg from "../assets/teamjoinbg.png"
+
+import { Link } from 'react-router-dom';
 
 const TeamJoin = () => {
   return (
-    <div className='teamjoin-container'>
+    <div className='teamjoin-container bg-img' style={{backgroundImage:`url(${teamjoinbg})`}}>
       <div className="teamjoin-left-container">
         <h2 className='teamjoin-left-title'>Want to join?</h2>
         <p className='teamjoin-left-description'>Find a team of digital marketers you can rely on. Every day, we build trust through communication, transparency, and results.</p>
       </div>
       <div className="teamjoin-right-container">
-        <button className='teamjoin-right-btn'>Learn More<FaArrowRight className='teamjoin-right-icon-btn' /></button>
+        <Link className='teamjoin-right-btn'>Learn More<FaArrowRight className='teamjoin-right-icon-btn' /></Link>
       </div>
     </div>
   )
